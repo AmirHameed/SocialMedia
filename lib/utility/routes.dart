f4f16d996e18e5b03fd8133a0bf353268ea73e01
Amir Hameed <amirhameed012@gmail.com>
2020-12-11 11:06:18 +0500

    1st

---
+++ lib/utility/routes.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:social_media/views/homeView.dart';
+
+class Routers {
+  static final route = [
+    GetPage(
+      name: '/homeView',
+      page: () => HomeView(),
+    ),
+
+  ];
+}
